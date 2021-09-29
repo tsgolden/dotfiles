32669cc26af0ea1b6b1d0a86a6c5a8cd6b0ea5ad
Trevor Golden <10249422+tsgolden@users.noreply.github.com>
2021-09-28 21:36:24 -0800

    Add script for bulk stowing by environment

---
+++ bulk-stow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -z "$1" ]; then
+    if [[ "$1" == "work" ]] || [[ "$1" == "personal" ]]; then
+        dirs=$(find . -maxdepth 1 -type d -printf '%f\n')
+        for dir in $dirs; do
+            # Ignore hidden files and directories
+            if [[ $dir != \.* ]]; then
+                if [[ "$1" == "work" ]] && [[ $dir == *work ]]; then
+                    stow "$dir"
+                elif [[ "$1" == "personal" ]] && [[ $dir == *personal ]]; then
+                    stow "$dir"
+                elif [[ $dir != *work ]] && [[ $dir != *personal ]]; then
+                    stow "$dir"
+                fi
+            fi
+        done
+    else
+        echo "Argument must be one of either \"work\" or \"personal\""
+    fi
+else
+    echo "No argument supplied; must be one of either \"work\" or \"personal\""
+fi
